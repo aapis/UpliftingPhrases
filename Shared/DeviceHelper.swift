6d16f8143afa2cb6eb47377014e538134a5cdbe6
Ryan Priebe <rpriebe@me.com>
2020-07-03 13:44:32 -0600

    nope, make this static

--- Shared/DeviceHelper.swift
+++ Shared/DeviceHelper.swift
@@ -9,23 +9,17 @@ import SwiftUI
 import Foundation
 
 class DeviceHelper {
-    private let device: UIDevice = UIDevice.current
+    private static let device: UIDevice = UIDevice.current
     
     public static func isiPad() -> Bool {
-        let helper = DeviceHelper()
-        
-        return helper.device.model == "iPad"
+        return device.model == "iPad"
     }
     
     public static func isiPhone() -> Bool {
-        let helper = DeviceHelper()
-        
-        return helper.device.model == "iPhone"
+        return device.model == "iPhone"
     }
     
     public static func isLandscape() -> Bool {
-        let helper = DeviceHelper()
-        
-        return helper.device.orientation.isLandscape
+        return device.orientation.isLandscape
     }
 }
